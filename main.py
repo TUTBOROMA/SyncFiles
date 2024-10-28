ccbdb7474e2766786fecd229113b9c9dd68a2204
Ravilov Roman Rinatovich <tutboroma7777@gmail.com>
2024-10-28 22:01:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 import time
 import os
 import logging
+import configparser
 from configparser import ConfigParser
 from yandex_disk import YandexDisk
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
+import requests
+import time
 # Чтение конфигурационного файла
 config = ConfigParser()
 config.read('config.ini', encoding='utf-8')
+def check_internet_connection():
+    try:
+        # Проверяем соединение с общедоступным сайтом (например, Google)
+        response = requests.get("https://www.google.com", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False
 
+def main():
+    while True:
+        if not check_internet_connection():
+            print("Интернета нет. Проверьте подключение.")
+        else:
+            print("Интернет подключен.")
+        
+        # Задержка перед следующей проверкой (например, 10 секунд)
+        time.sleep(10)
+logging.basicConfig(filename='sync_log.log', level=logging.ERROR)
 
+config = configparser.ConfigParser()
+config.read('config.ini', encoding='utf-8')
+
+SYNC_FOLDER = config.get('settings', 'sync_folder')
+
+# Проверка существования папки
+if not os.path.exists(SYNC_FOLDER):
+    error_message = f"Ошибка: Папка {SYNC_FOLDER} не существует."
+    print(error_message)
+    logging.error(error_message)
+else:
+    print(f"Папка {SYNC_FOLDER} найдена. Продолжаем работу.")
 SYNC_FOLDER = config.get('settings', 'sync_folder')
 REMOTE_FOLDER = config.get('settings', 'remote_folder')
 TOKEN = config.get('settings', 'token')
@@ -58,3 +89,4 @@ def monitor_folder():
 if __name__ == "__main__":
     logging.info("Запуск программы синхронизации")
     monitor_folder()
+    main()
